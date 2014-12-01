0957ba72544d9437119277d6305f98debbc7dddc
kk1311 <kk1311@messiah.edu>
2014-12-01 10:35:15 -0500

    send_mail.py
    Bug smash

--- send_mail.py
+++ send_mail.py
@@ -2,7 +2,7 @@ import smtplib, os
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEBase import MIMEBase
 from email.MIMEText import MIMEText
-from email.Utils import COMMASPACE, formatdate
+from email.utils import COMMASPACE, formatdate
 from email import Encoders
 
 def send_mail(send_from, send_to, subject, text, files=[], server="localhost"):
@@ -18,7 +18,7 @@ def send_mail(send_from, send_to, subject, text, files=[], server="localhost"):
     msg.attach( MIMEText(text) )
 
     for f in files:
-        part = MIMEBase('application', "octet-stream")
+        part = MIMEBase('application', 'base64')
         part.set_payload( open(f,"rb").read() )
         Encoders.encode_base64(part)
         part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(f))
